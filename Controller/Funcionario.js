656accce75d9ce4de6f64a3416d1430d9aea75cb
N1ckg4m3s <nico.ag.03@hotmail.com>
2024-11-08 15:31:14 -0300

    [ FIX ] Erro ao apagar o agendamento do profissional que vai ser apagado

--- Controller/Funcionario.js
+++ Controller/Funcionario.js
@@ -4,10 +4,10 @@
         Ultima_Atualização: ## (##/##/##);
 */
 const db = require('../Connect_DataBase');
-const Criptografia=require('../Criptografia');
+const Criptografia = require('../Criptografia');
 require('dotenv').config();
 
-const ChaveCrypto=process.env.Chave_Cripto_Funcionario
+const ChaveCrypto = process.env.Chave_Cripto_Funcionario
 
 function VerficarInjection(input) {
     const sqlInjectionPattern = /(\b(SELECT|INSERT|UPDATE|DELETE|DROP|--|\*|\bOR\b|\bAND\b)\b)/i;
@@ -16,7 +16,7 @@ function VerficarInjection(input) {
 
 async function Novo_Funcionario(req, res) {
     // Adiciona um novo funcionario
-    const { Nome, Acesso, Senha, Credenciais } = req.query
+    const { Nome, Acesso, Senha, Tipo_Acesso, Credenciais } = req.query
     try {
         if (!Nome) throw ('Sem nome');
         if (VerficarInjection(Nome)) throw ('Possivel ingection');
@@ -27,14 +27,18 @@ async function Novo_Funcionario(req, res) {
         if (!Senha) throw ('Sem Senha');
         if (VerficarInjection(Senha)) throw ('Possivel ingection');
 
+        if (!Tipo_Acesso) throw ('Sem Tipo_Acesso');
+        if (VerficarInjection(Tipo_Acesso)) throw ('Possivel ingection');
+
         const { Verificar_Credenciais, _ } = await Verificar_Credenciais_Funcionario(Credenciais)
         if (Verificar_Credenciais == undefined || Verificar_Credenciais) throw ('Credenciais Errada');
 
         const Result = await db.query(
-            'INSERT INTO meuEsquema.Profissional (Nome, Acesso, Senha) VALUES (@Nome, @Acesso, @Senha)', {
+            'INSERT INTO meuEsquema.Profissional (Nome, Acesso, Senha, Tipo_Acesso) VALUES (@Nome, @Acesso, @Senha, @Tipo_Acesso)', {
             Nome: Nome,
             Acesso: Acesso,
-            Senha: Senha
+            Senha: Senha,
+            Tipo_Acesso: Tipo_Acesso
         });
         return Result.rowsAffected[0] > 0 && res.status(200).send('Sucess');
     } catch (e) {
@@ -54,7 +58,7 @@ async function Remover_Funcionario(req, res) {
 
         if (Id != Funcionario_Id) throw ('Id Diferente');
 
-        const Ag_Result = await db.query('DELETE FROM meuEsquema.Agendamento WHERE Profissional_Id=@Id', { Id: Id });
+        const Ag_Result = await db.query('DELETE FROM meuEsquema.Trabalho_Profissional WHERE Profissional_Id=@Id', { Id: Id });
         const Cl_Result = await db.query('DELETE FROM meuEsquema.Profissional WHERE id=@Id', { Id: Id });
 
         if (Ag_Result.rowCount === 0 || Cl_Result.rowCount === 0) throw ('Nenhum registro encontrado para o Id fornecido');
@@ -77,16 +81,20 @@ async function Alterar_Informacoes_Funcionario(req, res) {
         if (!Mudancas.Senha) throw ('Sem Mudancas.Senha');
         if (VerficarInjection(Mudancas.Senha)) throw ('Possivel Ingection na Senha');
 
+        if (!Mudancas.Tipo_Acesso) throw ('Sem Mudancas.Tipo_Acesso');
+        if (VerficarInjection(Mudancas.Tipo_Acesso)) throw ('Possivel Ingection na Tipo_Acesso');
+
         const { Verificar_Credenciais, Funcionario_Id } = await Verificar_Credenciais_Cliente(Credenciais)
         if (Verificar_Credenciais == undefined || Verificar_Credenciais) throw ('Credenciais Errada');
 
         if (Funcionario_Id != Id) throw ('Ids Diferente');
 
         const Result = await db.query(
-            'UPDATE meuEsquema.Profissional SET Nome = @Nome, Senha = @Senha WHERE id = @id', {
+            'UPDATE meuEsquema.Profissional SET Nome = @Nome, Senha = @Senha, Tipo_Acesso = @Tipo_Acesso WHERE id = @id', {
             Id: 1,
             Nome: Mudancas.Nome,
             Senha: Mudancas.Senha,
+            Tipo_Acesso: Mudancas.Tipo_Acesso
         });
         return Result.rowsAffected[0] > 0 && res.status(200).send('Sucess');
     } catch (e) {
@@ -115,7 +123,7 @@ async function Verificar_Funcionario(req, res) {
                 Id: Rowrecordset.id,
                 Nome: Rowrecordset.Nome,
                 Acesso: Rowrecordset.Acesso,
-                Tipo_Acesso: -1
+                Tipo_Acesso: Rowrecordset.Tipo_Acesso
             }
 
             return res.status(200).json(Criptografia.Criptografar_Dados(JSON.stringify(Parametros), ChaveCrypto));
@@ -128,7 +136,7 @@ async function Verificar_Funcionario(req, res) {
 }
 
 async function Verificar_Credenciais_Funcionario(Credenciais) {
-    const Dados_Descriptografados = Criptografia.Descriptografar_Dados(Credenciais,ChaveCrypto)
+    const Dados_Descriptografados = Criptografia.Descriptografar_Dados(Credenciais, ChaveCrypto)
     if (!Dados_Descriptografados) return true
 
     const Dados_Parse = JSON.parse(Dados_Descriptografados)
